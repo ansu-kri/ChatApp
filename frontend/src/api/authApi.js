fa53addaf2fd37ce1d92c1726d2ea2f5ef7f4e1e
ansu-kri <ansukummari@gmail.com>
2026-04-30 21:47:29 +0530

    fix reduxtoolkit error

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,11 +1,37 @@
-import axios from "./axios";
+import api from "../app/api.jsx";
 
-export const loginUser = async (data) => {
-  const res = await axios.post("/auth/login", data);
-  return res.data;
-};
+export const authApi = api.injectEndpoints({
+  endpoints: (builder) => ({
 
-export const signupUser = async (data) => {
-  const res = await axios.post("/auth/signup", data);
-  return res.data;
-};
+    // ================= SIGNUP =================
+    signup: builder.mutation({
+      query: (formData) => ({
+        url: '/signup',
+        method: 'POST',
+      }),
+    }),
+
+    // ================= LOGIN =================
+    login: builder.mutation({
+      query: ({ email, password }) => ({
+        url: '/login',
+        method: 'POST',
+        body: { email, password },
+      }),
+    }),
+
+    // ================= LOGOUT =================
+    logout: builder.mutation({
+      query: () => ({
+        url: '/logout',
+        method: 'POST',
+      }),
+    }),
+  }),
+});
+
+export const {
+  useSignupMutation,
+  useLoginMutation,
+  useLogoutMutation,
+} = authApi;
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import api from './api.jsx';
 
-export const store= configureStore({
-    reducer:{
-        auth: authReducer,
-    }
-})
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware),
+});
