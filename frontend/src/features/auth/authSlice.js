72827e88214dd6ef4850b213ead5e44f975157ad
ansu-kri <ansukummari@gmail.com>
2026-05-15 21:22:42 +0530

    authslice

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -11,7 +11,7 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         setCredentials: (state, action)=>{
-            state.user = action.payload.user;
+            state.user = action.payload.user;    
             state.token= action.payload.token;
         },
         Logout: (state) => {
